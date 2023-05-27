cace9e676d16ea16584a3c0de86e67dfa8478e23
rlazic <118766710+rlazic@users.noreply.github.com>
2023-05-27 11:30:43 +0200

    Update drag_and_drop_list.js

--- drag_and_drop_list.js
+++ drag_and_drop_list.js
@@ -1,5 +1,5 @@
 const listItemsEl = document.querySelector(".listItems");
-const itemsEl = listItemsEl.querySelectorAll(".items");
+const itemsEl = listItemsEl.querySelectorAll(".item");
 
 itemsEl.forEach(item => {
 
@@ -8,3 +8,20 @@ itemsEl.forEach(item => {
     });
     item.addEventListener("dragend", () =>item.classList.remove("dragging"));
 });
+
+const initSortableList = (e) => {
+    e.preventDefault();
+    const draggingItem = document.querySelector(".dragging");
+
+    let siblings = [...listItemsEl.querySelectorAll(".item:not(.dragging")];
+
+    let nextSibling = siblings.find(sibling => {
+        return e.clientY <= sibling.offsetTop + sibling.offsetHeight / 2;
+
+    });
+
+    listItemsEl.insertBefore(draggingItem, nextSibling);
+}
+
+listItemsEl.addEventListener("dragover", initSortableList);
+listItemsEl.addEventListener("dragenter", e => e.preventDefault());
